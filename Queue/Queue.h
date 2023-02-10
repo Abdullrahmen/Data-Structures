e2a32bf0b0a9a9c06247da0ab2643161483f67ba
Abdullrahmen <abdullrahmen2003@gmail.com>
2023-02-10 13:37:10 +0200

    Initial queue commit

---
+++ Queue/Queue.h
@@ -0,0 +1,25 @@
+/*
+Fixed size queue using circular array trick.
+*/
+
+#ifndef QUEUE_H_
+#define QUEUE_H_
+#include <iostream>
+#include <memory>
+
+/// @brief Fixed size queue
+/// @tparam T 
+template<typename T>
+class Queue
+{
+private:
+    
+public:
+    Queue();
+    ~Queue();
+};
+
+
+
+#include "Queue.tpp"
+#endif // QUEUE_H_
---
+++ Queue/Tests.cpp
@@ -0,0 +1,6 @@
+#include "Queue.h"
+
+void test1()
+{
+    
+}
