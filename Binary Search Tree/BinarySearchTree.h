a12d8c464dd5d64507060db43d475769897deb37
Abdullrahmen <abdullrahmen2003@gmail.com>
2023-02-18 08:33:19 +0200

    Initial commit for Binary Search Tree

---
+++ Binary Search Tree/BinarySearchTree.h
@@ -0,0 +1,16 @@
+#ifndef BINARY_SEARCH_TREE_H_
+#define BINARY_SEARCH_TREE_H_
+
+#include <iostream>
+#include <memory>
+
+class BinarySearchTree
+{
+private:
+public:
+    BinarySearchTree();
+    ~BinarySearchTree();
+};
+
+#include "BinarySearchTree.tpp"
+#endif // BINARY_SEARCH_TREE_H_
